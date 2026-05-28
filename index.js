0b40773ba52bf47e207c7aa961da956b36ddb623
tarekul42 <tarekulrifat205@gmail.com>
2026-05-29 05:59:06 +0600

    feat: add global error handler and 404 middleware

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || 'Internal Server Error',
+  });
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: 'Route not found' });
+});
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jxobuiv.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -110,5 +124,5 @@ app.listen(port, () => {
  * app.put('/users/:id')
  * app.patch('/users/:id')
  * app.delete('/users/:id')
- * 
+ *
 */
